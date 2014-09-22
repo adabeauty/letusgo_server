8ebfc43dccca60b4242099f3feb03e0e9e3727a3
adabeauty <654296217@qq.com>
2014-09-23 00:38:39 +0800

    add delete

--- router/routes/goods.js
+++ router/routes/goods.js
@@ -1,24 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+var _ = require('lodash');
+
 var redis = require("redis");
 var client = redis.createClient();
 
 router.get('/', function(req, res){
 
     client.get('goods', function(req, obj){
-        res.send(obj);
+        res.send(JSON.parse(obj));
     });
 
 });
 
 router.post('/', function(req, res) {
 
-    var goods = req.param('goods');
-    client.set('goods', goods, function(err, obj){
+    var boughtGoods = req.param('goods');
+
+    client.set('goods', JSON.stringify(boughtGoods), function(err, obj){
         res.send(obj);
     });
 
 });
 
+router.delete('/:Id', function(req, res){
+
+    client.get('goods', function(err, obj){
+
+        var goods = JSON.parse(obj);
+        var deleteItem = JSON.parse(req.params.Id);
+        _.remove(goods, function(every){
+            return every.Id === deleteItem;
+        });
+
+        client.set('goods', JSON.stringify(goods), function(err, obj){
+            res.send(obj);
+        });
+    });
+});
 module.exports = router;
