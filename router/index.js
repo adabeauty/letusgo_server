970ec3fc2cdb3d4a2bb7002bb2d9c84a304e137c
adabeauty <654296217@qq.com>
2014-09-23 11:14:59 +0800

    delete clickCount

--- router/index.js
+++ router/index.js
@@ -2,5 +2,4 @@ module.exports = function(app) {
   app.use('/api/categories', require('./routes/categories'));
   app.use('/api/goods', require('./routes/goods'));
   app.use('/api/boughtGoods', require('./routes/boughtGoods'));
-  app.use('/api/clickCount', require('./routes/clickCount'));
 };
