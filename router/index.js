6bf2008f49535a031217dc2bb3946698a7969fdc
adabeauty <654296217@qq.com>
2014-09-25 23:48:24 +0800

    add payment

--- router/index.js
+++ router/index.js
@@ -2,4 +2,5 @@ module.exports = function(app) {
   app.use('/api/categories', require('./routes/categories'));
   app.use('/api/goods', require('./routes/goods'));
   app.use('/api/boughtGoods', require('./routes/boughtGoods'));
+  app.use('/api/payment', require('./routes/payment'));
 };
