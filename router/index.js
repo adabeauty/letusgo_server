60eb818e9049a0e58b366815d72cabad182e413d
adabeauty <654296217@qq.com>
2014-09-29 09:49:30 +0800

    create id file

--- router/index.js
+++ router/index.js
@@ -3,4 +3,5 @@ module.exports = function(app) {
   app.use('/api/goods', require('./routes/goods'));
   app.use('/api/cart', require('./routes/cart'));
   app.use('/api/payment', require('./routes/payment'));
+  app.use('/api/id', require('./routes/id'));
 };
